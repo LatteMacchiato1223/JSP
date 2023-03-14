40d6085f356e5fcf8b07b7a0f21e989e282bee97
Latte <126856463+LatteMacchiato1223@users.noreply.github.com>
2023-03-14 13:24:35 +0900

    Add files via upload

---
+++ Chapter06/src/sec01/ex01/InputServlet.java
@@ -0,0 +1,52 @@
+package sec01.ex01;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/input")
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id"); // html에서 아이디와 비번 호출 
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("ID : " + user_id);
+		System.out.println("PW : " + user_pw);
+		
+		String[] subject = request.getParameterValues("subject"); // 여러개 체크용 배열 
+		
+		System.out.print("Selected Subjects : ");
+		
+		for(String str : subject) {
+			System.out.print(str + " ");
+		}
+	}
+
+}
